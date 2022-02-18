51b8804ca3007d09506b710ec488297375ea00bc
heydi0116 <heydilishernandez@gmail.com>
2022-02-18 00:36:40 -0600

    first commit

---
+++ PM2E102/PM2E102/lista.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2E102
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class lista : ContentPage
+    {
+        public lista()
+        {
+            InitializeComponent();
+        }
+    }
+}
